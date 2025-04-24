c7574c3a3c99d81637cd3f57c0f1d97cb2313b99
Usernameorsmth <simidarawilliams@gmial.com>
2025-04-24 19:06:29 +0100

    Cooked

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-i = int(input("enter a number : "))
-if (i < 15):
-    print("i is smaller than 15")
-    print("i'm in if Block")
+number = int(input("Enter Number to check"))
+print("Number to be checked :", number)
+
+if number % 2 == 0:
+    print("This is an even number")
 else:
-    print("i is greater than 15")
-    print("i'm in else Block")
-print("i'm not in if and not in else Block")
+    print("This is an odd number")
